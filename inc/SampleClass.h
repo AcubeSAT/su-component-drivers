6ea2f8f4f2b5276c1bc557728238200444aa61b1
kpetridis24 <petridkon@gmail.com>
2021-05-16 16:56:02 +0300

    Configure the basic project

---
+++ inc/SampleClass.h
@@ -0,0 +1,20 @@
+/**
+ * As underlined in the README.md, you should change the below guards
+ * from "BASIC_PROJECT" etc, to match your specific task.
+ */
+#ifndef BASIC_ON_BOARD_SOFTWARE_PROJECT_TEMPLATE_SAMPLECLASS_H
+#define BASIC_ON_BOARD_SOFTWARE_PROJECT_TEMPLATE_SAMPLECLASS_H
+
+/**
+ * This is a doxygen-style comment that should exist in every header file. It has multiple purposes.
+ * The author should include in this comment:
+ * - The purpose of creating this class
+ * - A sample use
+ * - Various resources that the coder/reader can study to understand further the reasoning behind the functions
+ */
+class SampleClass {
+
+};
+
+
+#endif //BASIC_ON_BOARD_SOFTWARE_PROJECT_TEMPLATE_SAMPLECLASS_H
