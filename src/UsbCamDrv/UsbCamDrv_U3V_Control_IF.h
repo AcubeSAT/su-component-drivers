b0221f1fb71c3e5e95020cabee7efcb2681ebbee
fomarko <fomarkop@gmail.com>
2022-02-17 21:58:43 +0200

    New U3V interface files: Control, Event, Streaming  (empty)

---
+++ src/UsbCamDrv/UsbCamDrv_U3V_Control_IF.h
@@ -0,0 +1,51 @@
+//
+// Created by mojo on 17/02/22.
+//
+
+#ifndef COMPONENT_DRIVERS_USBCAMDRV_U3V_CONTROL_IF_H
+#define COMPONENT_DRIVERS_USBCAMDRV_U3V_CONTROL_IF_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "UsbCamDrv_DeviceClassSpec_U3V.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/********************************************************
+* Macro definitions
+*********************************************************/
+
+
+/********************************************************
+* Type definitions
+*********************************************************/
+
+
+/********************************************************
+* Constant declarations
+*********************************************************/
+
+
+/********************************************************
+* Variable declarations
+*********************************************************/
+
+
+/********************************************************
+* Function declarations
+*********************************************************/
+
+
+
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //COMPONENT_DRIVERS_USBCAMDRV_U3V_CONTROL_IF_H
