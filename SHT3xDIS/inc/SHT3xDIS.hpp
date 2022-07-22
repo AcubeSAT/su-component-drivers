bf9113dfcbe8bc653bacacb5909fffb810cbe490
fmoumtzi <fmoumtzi@csd.auth.gr>
2022-07-22 14:24:07 +0300

    more functionalities added

--- SHT3xDIS/inc/SHT3xDIS.hpp
+++ SHT3xDIS/inc/SHT3xDIS.hpp
@@ -3,7 +3,7 @@
 class SHT3xDIS {
 private:
 	/**
-	 * Register addresses are stored here.
+	 * Every register address.
 	 */
 	enum Register{
 		I2CAddr1 = 0x44, 
@@ -11,16 +11,32 @@ private:
  	};
 
  	/**
-	 * These are all the available commands for the single shoot mode.
+	 * All the available commands for the single shoot mode.
 	 */
-	enum Command {
+	enum Measurment {
 	     HIGH_ENABLED = 0x2C06, 
 	     MEDIUM_ENABLED = 0x2C0D,  
 	     LOW_ENABLED = 0x2C10,
 	     HIGH_DISABLED = 0x2400, 
 	     MEDIUM_DISABLED = 0x240B,  
-	     LOW_DISABLED = 0x2416 
-	     softReset = 0x30A2;   
+	     LOW_DISABLED = 0x2416   
+	};
+
+	/**
+	 * Commands for Heater
+	 */
+	enum Heater{
+		HEATER_ENABLED = 0x306D,
+		HEATER_DISABLED = 0x3066
+	};
+
+	/**
+	 * All commands gor status register.
+	 */
+	enum StatusRegister
+	{
+		READ_STATUS_REGISTER = 0xF32D;
+		CLEAR_STATUS_REGISTER = 0x3041;
 	};
 
 	/**
@@ -38,7 +54,37 @@ public:
 	/**
 	 * Writes a command to register so that it starts the measurement
 	 */
-	void writeCommandtoRegister(Register register, Command command);
+	void writeCommandtoRegister(Register register, uint16_t command);
+
+	/**
+	 * Sets the type of measurement the sensor will execute.
+	 */
+	void setMeasurement(Register register, Measurment command);
+
+	/**
+	 * Sets the heater(On/Off).
+	 */
+	void setHeater(Register register, Heater command);
+
+	/**
+	 * Writs a command to the Status register so it reads from it or clears it.
+	 */
+	void setStatusRegisterCommand(Register register, SHT3xDIS::StatusRegister command);
+
+	/**
+	 * Reads the data sent by the status register.
+	 */
+	etl::array<uint16_t, 2> readStatusRegister(Register register)
+
+	/**
+	 * Performs a soft reset to the sensor.
+	 */
+	void setSoftReset(Register register);
+
+	/**
+	 * Performs a hard reset on the whole I2C Bus.
+	 */
+	void HardReset();
 
 	/**
 	 * Tranforms the raw temperature that is measured into the physical.
--- SHT3xDIS/src/SHT3xDIS.cpp
+++ SHT3xDIS/src/SHT3xDIS.cpp
@@ -1,6 +1,6 @@
 #include "SHT3xDIS.hpp"
 
-void SHT3xDIS::writeCommandtoRegister(Register register, Command command){
+void SHT3xDIS::writeCommandtoRegister(Register register, uint16_t command){
     uint8_t txData[] = {
             register,
             static_cast<uint8_t>(command >> 8),
@@ -33,12 +33,62 @@ etl::array<float, 2> SHT3xDIS::readMeasurements(Register register){
         rawTemperature = (data[0]<<8) | data[1];
         rawHumidity = (data[3]<<8) | data[4];
 
+        measurements[0] = getTemperature(rawTemperature);
+        measurements[1] = getHumidity(rawHumidity);
+
+        return measurements;
     } else {
-        return 0;
+        return NULL;
+    }
+}
+
+void setMeasurement(Register register, SHT3xDIS::Measurment command){
+    writeCommandtoRegister(register, command);
+}
+
+void setHeater(Register register, SHT3xDIS::Heater command){
+    writeCommandtoRegister(register, command);
+}
+
+void setStatusRegisterCommand(Register register, SHT3xDIS::StatusRegister command){
+    writeCommandtoRegister(register, command);
+}
+
+etl::array<uint16_t, 2> SHT3xDIS::readStatusRegister(Register register){
+    uint8_t data[4];
+    etl::array<uint16_t, 2> status = {}; 
+
+    if (TWIHS2_Read(register, data, 4)) {
+        while (TWIHS2_IsBusy());
+        error = TWIHS2_ErrorGet();
+
+        status[0] = data[1];
+        status[1] = data[2];
+
+        return status;
+    } else {
+        return NULL;
+    }
+}
+
+void setSoftReset(Register register){
+    uint16_t softReset = 0x30A2;
+    writeCommandtoRegister(register, softReset);
+}
+
+void SHT3xDIS::HardReset(){
+    resetCommand = 0x0006;
+
+    uint8_t txData[] = { static_cast<uint8_t>(resetCommand >> 8), static_cast<uint8_t>(resetCommand & 0x00FF) };
+    uint8_t ackData = 0;
+
+    if (TWIHS2_Write(register, &ackData, 1)) {
+    while (TWIHS2_IsBusy());
+    error = TWIHS2_ErrorGet();
     }
 
-    measurements[0] = getTemperature(rawTemperature);
-    measurements[1] = getHumidity(rawHumidity);
-
-    return measurements;
+    if (TWIHS2_Write(register, txData, 2)) {
+    while (TWIHS2_IsBusy());
+    error = TWIHS2_ErrorGet();
+    }
 }
