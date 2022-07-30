22bca649c8b9f1bf5fe170ef4853a84e882f1d52
Bozatzis Miltiadis <mpozan20@gmail.com>
2022-07-30 21:14:30 +0200

    Add preliminary documentation and change the way led pins are stored

--- LEDBoard/LEDBoard.hpp
+++ LEDBoard/LEDBoard.hpp
@@ -2,24 +2,41 @@
 #define COMPONENT_DRIVERS_LEDBOARD_HPP
 
 #include "definitions.h"
+#include <etl/array.h>
 
-
+/**
+ * Excitation LED Board driver
+ *
+ * This is a simple driver for the in-house Excitation LED PCB of AcubeSAT
+ * More details about the PCB can be found at:
+ * https://gitlab.com/acubesat/su/optics/excitation-led-pcb
+ *
+ * @author Miltiadis Bozatzis <mpozan20@gmail.com>
+ */
 class LEDBoard{
 private:
 
-    enum LED_PIN : PIO_PIN {
-        LED_1_PIN = PD3,
-        LED_2_PIN = PD5,
-        LED_3_PIN = PD7,
-        LED_4_PIN = PC19
-    };
+    const uint8_t numberOfLeds = 4;
 
+    /**
+     * Array of the pins connected to the LEDs of the Board
+     */
+    etl::array<PIO_PIN, numberOfLeds> ledPins;
 
 public:
 
-    LEDBoard();
+    /**
+     * Constructor to set the initial LED intensities to zero
+     * @param ledPins Pointer to an array with the pins connected to the LEDs
+     */
+    LEDBoard(PIO_PIN *ledPins);
 
-    void setLedIntensity(LED_PIN pin, uint8_t letIntensity);
+    /**
+     * Set the LED intensities
+     * @param ledIndex The index of the LED (1,2,3,4)
+     * @param letIntensity The value of the intensity
+     */
+    void setLedIntensity(uint8_t ledIndex, uint8_t ledIntensity);
 
 };
 
