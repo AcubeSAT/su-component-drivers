f735780ea7a52931908281b7f57cdbe1093bd494
Bozatzis Miltiadis <mpozan20@gmail.com>
2022-08-07 21:10:25 +0200

    Add PWM functions to have various levels of light intensity

--- LEDBoard/inc/LEDBoard.hpp
+++ LEDBoard/inc/LEDBoard.hpp
@@ -26,21 +26,52 @@ private:
      */
     etl::array<PIO_PIN, numberOfLeds> ledPins;
 
+    /**
+     * Array of the PWM Channels connected to each pin
+     * each pin ledPins[i] should correspond to PWM Channel pinPwmChannels[i]
+     */
+    etl::array<PWM_CHANNEL_NUM, numberOfLeds> pinPwmChannels;
+
+    /**
+     * Array of the PWM Channel Masks for each channel
+     */
+    etl::array<PWM_CHANNEL_MASK, numberOfLeds> pinPwmChannelMasks;
+
+    /**
+     * Array of the PWM Handler of each pin
+     * each pin ledPins[i] should correspond to PWM Handler pinPwmChannels[i]
+     */
+    etl::array<bool, numberOfLeds> pwmHandlers;
+
 public:
 
     /**
      * Constructor to set the initial LED intensities to zero and define the LED pins
      * @param pins Pointer to an array with the pins connected to the LEDs
+     * @param channels Pointer to an array with the PWM channel of each LED Pin
+     * @param handlers Pointer to an array with the PWM handler of each LED Pin
      */
-    LEDBoard(PIO_PIN *pins);
+    LEDBoard(PIO_PIN *pins, PWM_CHANNEL_NUM *channels, bool *handlers);
 
     /**
      * Set the LED intensities
      * @param ledIndex The index of the LED (1,2,3,4)
-     * @param letIntensity The value of the intensity (boolean)
-     * @todo Modify this function to accept various ledIntensity values and change the LED voltage accordingly with PWM
+     * @param ledValue The value of the led, boolean (on-true, off-false)
      */
-    void setLedIntensity(uint8_t ledIndex, bool ledIntensity);
+    void ledWrite(uint8_t ledIndex, bool ledValue);
+
+    /**
+     * Set the LED intensities
+     * @param ledIndex The index of the LED (1,2,3,4)
+     * @param letIntensity The value of the intensity (percentage from 0 to 100)
+     */
+    void setLedIntensity(uint8_t ledIndex, uint16_t intensity);
+
+    /**
+     * Stops the PWM on the selected LED pin
+     * @param ledIndex The index of the LED (1,2,3,4)
+     */
+    void closeLed(uint8_t ledIndex);
 
 };
 
--- LEDBoard/src/LEDBoard.cpp
+++ LEDBoard/src/LEDBoard.cpp
@@ -1,19 +1,67 @@
 #include "../inc/LEDBoard.hpp"
 
-LEDBoard::LEDBoard(PIO_PIN *pins) {
+LEDBoard::LEDBoard(PIO_PIN *pins, PWM_CHANNEL_NUM *channels, bool *handlers) {
     for(uint8_t i=0; i<numberOfLeds; i++){
         ledPins[i] = pins[i];
+        pinPwmChannels[i] = channels[i];
+        pwmHandlers[i] = handlers[i];
         PIO_PinWrite(ledPins[i], true);
+
+        if(pinPwmChannels[i] == PWM_CHANNEL_0)
+            pinPwmChannelMasks[i] = PWM_CHANNEL_0_MASK;
+        else if(pinPwmChannels[i] == PWM_CHANNEL_1)
+            pinPwmChannelMasks[i] = PWM_CHANNEL_1_MASK;
+        else if(pinPwmChannels[i] == PWM_CHANNEL_2)
+            pinPwmChannelMasks[i] = PWM_CHANNEL_2_MASK;
+        else
+            pinPwmChannelMasks[i] = PWM_CHANNEL_3_MASK;
     }
+
+    PWM0_Initialize();
+    PWM1_Initialize();
 }
 
-void LEDBoard::setLedIntensity(uint8_t ledIndex, bool ledIntensity) {
+void LEDBoard::ledWrite(uint8_t ledIndex, bool ledValue) {
     PIO_PIN ledPin;
     if(ledIndex <= 4 && ledIndex > 0){
-        PIO_PIN = ledPins[ledIndex];
+        ledPin = ledPins[ledIndex];
     }
     else return;
 
-    PIO_PinWrite(ledPin, !ledIntensity);
+    PIO_PinWrite(ledPin, !ledValue);
 }
+
+void LEDBoard::setLedIntensity(uint8_t ledIndex, uint16_t intensity) {
+    if(ledIndex <= 4 && ledIndex > 0) {
+        if (pwmHandlers[ledIndex]) {
+            uint16_t period = PWM1_ChannelPeriodGet(pinPwmChannels[ledIndex]);
+            uint16_t dutyCycle = period * intensity / 100;
+            PWM1_ChannelDutySet(pinPwmChannels[ledIndex], uint16_t
+            dutyCycle);
+            PWM1_ChannelsStart(pinPwmChannelMasks[ledIndex]);
+        }
+        else {
+            uint16_t period = PWM0_ChannelPeriodGet(pinPwmChannels[ledIndex]);
+            uint16_t dutyCycle = period * intensity / 100;
+            PWM0_ChannelDutySet(pinPwmChannels[ledIndex], uint16_t
+            dutyCycle);
+            PWM0_ChannelsStart(pinPwmChannelMasks[ledIndex]);
+        }
+    }
+    else return;
+}
+
+void LEDBoard::closeLed(uint8_t ledIndex) {
+    if(ledIndex <= 4 && ledIndex > 0) {
+        if (pwmHandlers[ledIndex]) {
+            PWM1_ChannelsStop(pinPwmChannelMasks[ledIndex]);
+        }
+        else {
+            PWM0_ChannelsStop(pinPwmChannelMasks[ledIndex]);
+        }
+    }
+    else return;
+}
+
+
 
