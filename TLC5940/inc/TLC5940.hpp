0eefce8983ac21037ae9e5289dbb42d16737d234
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-06-06 14:43:43 +0300

    Created TLC5940 Functions

--- TLC5940/inc/TLC5940.hpp
+++ TLC5940/inc/TLC5940.hpp
@@ -1,8 +1,33 @@
-#pragma once
-
-#include <stdint.h> /// For uint8_t and uint16_t types
+#include <SPI.h>
+#include <stdint.h>
+#include <etl/array.h>
 
 class TLC5940 {
+public:
+  static constexpr size_t MaxChannels = 16;
+  static constexpr size_t MaxDrivers = 4;
 
+  TLC5940(uint8_t numDrivers, uint8_t latchPin, uint8_t blankPin, uint8_t dcPin, uint8_t spiClock, uint8_t spiData);
+
+  void begin();
+  void setPWM(uint16_t channel, uint16_t value);
+  void setAllPWM(uint16_t value);
+  void update();
+
+private:
+  uint8_t numDrivers;
+  uint16_t numChannels;
+  uint16_t numPWM;
+  uint8_t latchPin;
+  uint8_t blankPin;
+  uint8_t dcPin;
+  uint8_t spiClock;
+  uint8_t spiData;
+  etl::array<uint16_t, MaxChannels * MaxDrivers> pwmData;
+
+  void spiSend(uint16_t data);
+  void writeData();
+  void setLatch();
+  void setBlank();
 
 };
--- TLC5940/src/TLC5940.cpp
+++ TLC5940/src/TLC5940.cpp
@@ -1,3 +1,39 @@
 #include "TLC5940.hpp"
 
 
+TLC5940::TLC5940(uint8_t numDrivers, uint8_t latchPin, uint8_t blankPin, uint8_t dcPin, uint8_t spiClock,
+                 uint8_t spiData) {
+
+}
+
+void TLC5940::begin() {
+
+}
+
+void TLC5940::setPWM(uint16_t channel, uint16_t value) {
+
+}
+
+void TLC5940::setAllPWM(uint16_t value) {
+
+}
+
+void TLC5940::update() {
+
+}
+
+void TLC5940::spiSend(uint16_t data) {
+
+}
+
+void TLC5940::writeData() {
+
+}
+
+void TLC5940::setLatch() {
+
+}
+
+void TLC5940::setBlank() {
+
+}
