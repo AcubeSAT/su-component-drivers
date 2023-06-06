3bf40d887f5d1c158c9ce4b614b79f37cf017e8b
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-06-06 20:01:26 +0300

    Implemented the funtions

--- TLC5940/inc/TLC5940.hpp
+++ TLC5940/inc/TLC5940.hpp
@@ -1,33 +1,90 @@
-#include <SPI.h>
-#include <stdint.h>
+#include <cstdint>
 #include <etl/array.h>
 
+/**
+ * TLC5940 class for controlling TLC5940
+ */
 class TLC5940 {
 public:
-  static constexpr size_t MaxChannels = 16;
-  static constexpr size_t MaxDrivers = 4;
 
-  TLC5940(uint8_t numDrivers, uint8_t latchPin, uint8_t blankPin, uint8_t dcPin, uint8_t spiClock, uint8_t spiData);
+    /**
+     * Maximum number of channels per TLC5940 driver
+     */
+    static constexpr size_t MaxChannels = 16;
 
-  void begin();
-  void setPWM(uint16_t channel, uint16_t value);
-  void setAllPWM(uint16_t value);
-  void update();
+    /**
+     * Maximum number of TLC5940 drivers
+     */
+    static constexpr size_t MaxDrivers = 1;
+
+    /**
+     * @brief TLC5940 constructor
+     * @param latchPin Pin connected to the LATCH (LAT) pin of the TLC5940
+     * @param blankPin Pin connected to the BLANK (BLK) pin of the TLC5940
+     */
+    TLC5940(PIO_Pin latchPin, PIO_Pin blankPin);
+
+    /**
+     * @brief Initialize the TLC5940
+     */
+    void begin();
+
+    /**
+     * @brief Set the PWM value for a specific channel
+     * @param channel Channel index (0 to MaxChannels - 1)
+     * @param value PWM value (0 to 4095)
+     */
+    void setPWM(uint8_t channel, uint16_t value);
+
+    /**
+     * @brief Set the PWM value for all channels
+     * @param value PWM value (0 to 4095)
+     */
+    void setAllPWM(uint16_t value);
+
+    /**
+     * @brief Update the TLC5940 to apply the changes made to the PWM values
+     */
+    void update();
 
 private:
-  uint8_t numDrivers;
-  uint16_t numChannels;
-  uint16_t numPWM;
-  uint8_t latchPin;
-  uint8_t blankPin;
-  uint8_t dcPin;
-  uint8_t spiClock;
-  uint8_t spiData;
-  etl::array<uint16_t, MaxChannels * MaxDrivers> pwmData;
 
-  void spiSend(uint16_t data);
-  void writeData();
-  void setLatch();
-  void setBlank();
+    /**
+     * @brief Send data to the TLC5940 via SPI
+     * @param data Data to be sent
+     */
+    void sendSPI(uint16_t data);
+
+    /**
+     * @brief Write the PWM data to the TLC5940
+     */
+    void writeData();
+
+    /**
+     * @brief Set the XLAT (LATCH) pin to latch the PWM data
+     */
+    void latchData();
+
+    /**
+     * @brief Set the BLANK pin to apply the PWM data to the outputs
+     */
+    void setBlank(bool enableBlank);
+
+private:
+
+    /**
+     * XLAT pin
+     */
+    PIO_Pin latchPin;
+
+    /**
+     * BLANK pin
+     */
+    PIO_Pin blankPin;
+
+    /**
+     * PWM data array
+     */
+    etl::array<uint16_t, MaxChannels * MaxDrivers> pwmData;
 
 };
--- TLC5940/src/TLC5940.cpp
+++ TLC5940/src/TLC5940.cpp
@@ -1,39 +1,62 @@
 #include "TLC5940.hpp"
 
-
-TLC5940::TLC5940(uint8_t numDrivers, uint8_t latchPin, uint8_t blankPin, uint8_t dcPin, uint8_t spiClock,
-                 uint8_t spiData) {
-
+TLC5940::TLC5940(PIO_PIN latchPin, PIO_PIN blankPin) : latchPin(latchPin), blankPin(blankPin) {
+    PIO_PinWrite(latchPin, false);
+    PIO_PinWrite(blankPin, true);
+    setAllPWM(static_cast<uint16_t>(0))
 }
 
 void TLC5940::begin() {
-
+    writeData();
+    latchData();
+    setBlank(false);
 }
 
-void TLC5940::setPWM(uint16_t channel, uint16_t value) {
+void TLC5940::setPWM(uint8_t channel, uint16_t value) {
+    // Check for invalid channel number
+    if (channel >= MaxChannels)
+        return;
 
+    // Set the PWM value for the specified channel
+    pwmData.at(channel) = value;
 }
 
 void TLC5940::setAllPWM(uint16_t value) {
-
+    void TLC5940::setAllPWM(uint16_t value) {
+        // Set the same PWM value for all channels
+        for (size_t i = 0; i < numChannels; i++) {
+            pwmData.at(i) = value;
+        }
+    }
 }
 
 void TLC5940::update() {
-
+    setBlank(true);
+    begin();
 }
 
-void TLC5940::spiSend(uint16_t data) {
+void TLC5940::sendSPI(uint16_t data) {
+    // Convert the 16-bit data into an array of bytes
+    uint8_t txData[2];
+    txData[0] = static_cast<uint8_t>(data >> 8); // MSB 8-15
+    txData[1] = static_cast<uint8_t>(data & 0xFF); // LSB 0-7
 
+    // Send the data and ignore any received data
+    SPI1_WriteRead(txData, sizeof(txData), nullptr, 0);
 }
 
 void TLC5940::writeData() {
-
+    // Iterate over the stored PWM data and send it to the TLC5940 chip
+    for (uint16_t data : pwmData) {
+        sendSPI(data);
+    }
 }
 
-void TLC5940::setLatch() {
-
+void TLC5940::setBlank(bool enableBlank) {
+    PIO_PinWrite(blankPin, enableBlank);
 }
 
-void TLC5940::setBlank() {
-
+void TLC5940::latchData() {
+    PIO_PinWrite(latchPin, true);
+    PIO_PinWrite(latchPin, false);
 }
