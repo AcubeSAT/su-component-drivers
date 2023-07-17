c9af7cb1c39a8135105708216cb4b887c6192c7c
Dimitrios Sourlantzis <jem2010@windowslive.com>
2023-07-17 19:18:49 +0000

    Implement SHT3xDIS driver.

--- README.md
+++ README.md
@@ -4,6 +4,29 @@ This repository will be added as a git-submodule.
 
 SpaceDot members: don't forget to check the guidelines [here](https://mm.spacedot.gr/acubesat/pl/i9qt3t7cyirbxysramwzybk1se)
 
-## Drivers Implemented
+# Drivers Implemented
+
+## Camera Driver
 
 - [Camera Driver](https://gitlab.com/acubesat/su/on-board-software/su-component-drivers/-/tree/master/U3VCamDriver) and its [wikipage](https://gitlab.com/groups/acubesat/su/on-board-software/-/wikis/USB3-Vision-Camera-Driver)
+
+## Humidity Sensor Driver
+
+SHT3x-DIS Driver (only the single-shot mode)
+
+[Datasheet](https://sensirion.com/media/documents/213E6A3B/63A5A569/Datasheet_SHT3x_DIS.pdf)
+
+If you are using the SHT3x-DIS evaluation board along with a SAMV71 Xplained Ultra evaluation kit, follow the pinout below.
+You can use the `TW0` pins on the J500 connector or the `TW2` pins on the J505 connector.
+
+
+| Pin Number | Name   | Comments                                                                                                                              |
+| ------ |--------|---------------------------------------------------------------------------------------------------------------------------------------|
+| 1 | SDA    | Connect to TWD of ATSAMV71                                                                                                            |
+| 2 | ADDR   | Connect to GND pin of ATSAMV71 or use a breadboard for a common GND                                                                   |
+| 3 | Alert  | Can be left floating. If required for testing purposes, connect to any GPIO, enable the GPIO as input and activate the GPIO interrupt |
+| 4 | SCL    | Connect to TWCK of ATSAMV71                                                                                                           |
+| 5 | VDD    | Connect to 3V3 of the ATSAMV71 power headers                                                                                          |
+| 6 | nRESET | Can be left floating. If required for testing purposes, connect to any GPIO and activate the GPIO as output                           |
+| 7 | R      | Connect to GND pin of ATSAMV71 or to the common GND                                                                                   |
+| 8 | VSS    | Connect to GND pin of ATSAMV71 or to the common GND                                                                                                         |
---
+++ SHT3xDIS/src/SHT3xDIS.cpp
@@ -0,0 +1,111 @@
+#include "SHT3xDIS.hpp"
+
+bool SHT3xDIS::crc8(uint8_t msb, uint8_t lsb, uint8_t checksum) {
+    uint8_t CRC = 0xFF;
+    uint8_t polynomial = 0x31;
+
+    CRC ^= msb;
+    for (uint8_t index = 0; index < 8; ++index) {
+        CRC = (CRC & 0x80) ? ((CRC & 0x7F) << 1) ^ polynomial : (CRC & 0x7F) << 1;
+    }
+
+    CRC ^= lsb;
+    for (uint8_t index = 0; index < 8; ++index) {
+        CRC = (CRC & 0x80) ? ((CRC & 0x7F) << 1) ^ polynomial : (CRC & 0x7F) << 1;
+    }
+
+    return CRC == checksum;
+}
+
+template<typename F, typename... Arguments>
+bool SHT3xDIS::executeI2CTransaction(F i2cFunction, Arguments... arguments) {
+    if (i2cFunction(I2CAddress, arguments...)) {
+        waitForI2CBuffer();
+        checkForNACK();
+        return true;
+    } else {
+        LOG_INFO << "Humidity sensor with address " << I2CAddress << ": I2C bus is busy";
+        return false;
+    }
+}
+
+void SHT3xDIS::checkForNACK() {
+    auto error = SHT3xDIS_TWIHS_ErrorGet();
+    if (error == TWIHS_ERROR_NACK) {
+        LOG_ERROR << "Humidity-Temperature sensor with address " << I2CAddress << " , is disconnected, suspending task";
+        vTaskSuspend(nullptr);
+    }
+}
+
+void SHT3xDIS::sendCommandToSensor(uint16_t command) {
+    etl::array<uint8_t, NumberOfBytesInCommand> commandBytes{};
+    splitHalfWordToByteArray(commandBytes, command);
+    executeI2CTransaction(SHT3xDIS_TWIHS_Write, commandBytes.data(), NumberOfBytesInCommand);
+}
+
+void
+SHT3xDIS::executeWriteReadTransaction(etl::array<uint8_t, NumberOfBytesOfStatusRegisterWithCRC> &statusRegisterData) {
+    etl::array<uint8_t, NumberOfBytesInCommand> commandBytes{};
+
+    splitHalfWordToByteArray(commandBytes, StatusRegisterCommands::READ);
+
+    if (not executeI2CTransaction(SHT3xDIS_TWIHS_WriteRead, commandBytes.data(), NumberOfBytesInCommand,
+                                  statusRegisterData.data(), NumberOfBytesOfStatusRegisterWithCRC)) {
+        return;
+    }
+
+    if constexpr (UseCRC) {
+        if (not crc8(statusRegisterData[0], statusRegisterData[1], statusRegisterData[2])) {
+            LOG_ERROR << "Error in Humidity Sensor Status Register checksum";
+        }
+    }
+}
+
+void SHT3xDIS::readSensorDataSingleShotMode(etl::array<uint8_t, NumberOfBytesOfMeasurementsWithCRC> &sensorData) {
+    if (not executeI2CTransaction(SHT3xDIS_TWIHS_Read, sensorData.data(), NumberOfBytesOfMeasurementsWithCRC)) {
+        return;
+    }
+
+    if constexpr (UseCRC) {
+        if (not crc8(sensorData[0], sensorData[1], sensorData[2])) {
+            LOG_ERROR << "Error in Humidity Sensor temperature value checksum";
+        }
+
+        if (not crc8(sensorData[3], sensorData[4], sensorData[5])) {
+            LOG_ERROR << "Error in Humidity Sensor humidity value checksum";
+        }
+    }
+
+}
+
+etl::pair<float, float> SHT3xDIS::getOneShotMeasurement(SingleShotModeCommands command) {
+    etl::array<uint8_t, NumberOfBytesOfMeasurementsWithCRC> sensorData{};
+    sendCommandToSensor(command);
+    vTaskDelay(pdMS_TO_TICKS(msToWait));
+
+    readSensorDataSingleShotMode(sensorData);
+    return {convertRawTemperatureValueToPhysicalScale(concatenateTwoBytesToHalfWord(sensorData[0], sensorData[1])),
+            convertRawHumidityValueToPhysicalScale(concatenateTwoBytesToHalfWord(sensorData[3], sensorData[4]))};
+}
+
+void SHT3xDIS::setHeater(SHT3xDIS::HeaterCommands command) {
+    sendCommandToSensor(command);
+}
+
+void SHT3xDIS::clearStatusRegister() {
+    sendCommandToSensor(StatusRegisterCommands::CLEAR);
+}
+
+uint16_t SHT3xDIS::readStatusRegister() {
+    etl::array<uint8_t, NumberOfBytesOfStatusRegisterWithCRC> statusRegisterData{};
+
+    executeWriteReadTransaction(statusRegisterData);
+
+    return concatenateTwoBytesToHalfWord(statusRegisterData[0], statusRegisterData[1]);
+}
+
+void SHT3xDIS::performSoftReset() {
+    PIO_PinWrite(NResetPin, false);
+    vTaskDelay(pdMS_TO_TICKS(msToWait));
+    PIO_PinWrite(NResetPin, true);
+}
