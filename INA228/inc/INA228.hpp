350f94dd72778fff7831aaaf88086c9ca842cbdd
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-07-22 22:43:14 +0300

    Added an enum class for the registers addresses

--- INA228/inc/INA228.hpp
+++ INA228/inc/INA228.hpp
@@ -1,11 +1,42 @@
 #pragma once
 
+#include <cstdint>
+
+/**
+ * @brief C++ class for interfacing with the INA228 Current Monitor.
+ *
+ * This class provides functions to read various electrical parameters from the INA228 device,
+ * such as voltage, temperature, current, power, energy, and charge.
+ */
 class INA228 {
+public:
 
-	public:
+private:
 
-	private:
+	/**
+     * @brief Enumeration that holds the addresses of the INA228 registers
+     */
+    enum class RegisterAddress : uint8_t {
+        CONFIG = 0x00,
+        ADC_CONFIG = 0x01,
+        SHUNT_CAL = 0x02,
+        SHUNT_TEMPCO = 0x03,
+        VSHUNT = 0x04,
+        VBUS = 0x05,
+        DIETEMP = 0x06,
+        CURRENT = 0x07,
+        POWER = 0x08,
+        ENERGY = 0x09,
+        CHARGE = 0x0A,
+        DIAG_ALRT = 0x0B,
+        SOVL = 0x0C,
+        SUVL = 0x0D,
+        BOVL = 0x0E,
+        BUVL = 0x0F,
+        TEMP_LIMIT = 0x10,
+        PWR_LIMIT = 0x11,
+        MANUFACTURER_ID = 0x3E,
+        DEVICE_ID = 0x3F
+    };
 
 };
-
-
