73bbe37e7179830e457e97a6fa24e18265a95adb
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-07-26 18:23:51 +0300

    Created getCurrent and getPower functions

--- INA228/inc/INA228.hpp
+++ INA228/inc/INA228.hpp
@@ -11,10 +11,57 @@
 class INA228 {
 public:
 
+    /**
+     * @brief Constructor for the INA228 class.
+     * @param something The I2C master port number used for communication.
+     */
+    constexpr INA228();
+
+    /**
+     * @brief Reads the current value from the INA228 device.
+     * @return The current value in amperes.
+    */
+    float getCurrent();
+
+    /**
+     * @brief Reads the power value from the INA228 device.
+     * @return The power value in watts.
+     */
+    float getPower();
+
+
 private:
 
+    /**
+     * @brief The maximum expected current, used to calculate CurrentLSB
+     */
+    static constexpr float MaximumExpectedCurrent = 500;
+
+    /**
+     * The LSB step size for the CURRENT register where the current in Amperes is stored
+     *
+     * @brief While the smallest CurrentLSB value yields highest resolution,
+     * it is common to select a higher round-number (no higher than 8x) value for the CurrentLSB
+     * in order to simplify the conversion of the CURRENT
+     */
+    static constexpr float CurrentLSB = MaximumExpectedCurrent / static_cast<float>(1 << 19);
+
+    /**
+     * Value of current-sensing resistor
+     */
+    static constexpr float RShuntResistor = 100;
+
+    /**
+     * Value that is going to be written in SHUNT_CAL register
+     *
+     * @brief The current is calculated following a shunt voltage measurement based on the value set
+     * in the SHUNT_CAL register. If the value loaded into the SHUNT_CAL register is zero, the current
+     * value reported through the CURRENT register is also zero
+     */
+    static constexpr float SHUNT_CAL = 13107.2 * 1000000 * CurrentLSB * RShuntResistor;
+
 	/**
-     * @brief Enumeration that holds the addresses of the INA228 registers
+     * @enum Holds the addresses of the INA228 registers
      */
     enum class RegisterAddress : uint8_t {
         CONFIG = 0x00,
--- INA228/src/INA228.cpp
+++ INA228/src/INA228.cpp
@@ -1,4 +1,23 @@
 #include "INA228.hpp"
 
+constexpr INA228::INA228() {
 
+}
+
+float INA228::getCurrent() {
+    int32_t current = readFromRegister();
+
+    uint8_t sign = current & (1 << 12);
+
+    if(sign == 1)
+        current = !current;
+
+    return current * CurrentLSB;
+}
+
+float INA228::getPower() {
+    uint32_t power = static_cast<uint32_t>(readRegister());
+
+    return 3.2f * CurrentLSB * static_cast<float>(power) / 256.0f;
+}
 
