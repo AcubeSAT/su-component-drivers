14f952f024ecd4784478038fdfec7289ec459539
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2023-11-11 17:30:31 +0200

    Small log change

--- PCA9685/src/PCA9685.cpp
+++ PCA9685/src/PCA9685.cpp
@@ -11,13 +11,14 @@ bool PCA9685::i2cReadData(RegisterAddresses registerAddress, uint8_t *rData, uin
 
     while (true) {
         if (TWIHS2_IsBusy()) {
+            LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus is busy";
             continue;
         } else {
             if (PCA9685_TWIHS_WriteRead(slaveAddressRead, reinterpret_cast<uint8_t *>(&registerAddress), 1, rData,
                                         numberOfBytesToRead)) {
                 return true;
             } else {
-                LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus is busy";
+                LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus NAK";
                 return false;
             }
         }
@@ -29,12 +30,13 @@ bool PCA9685::i2cWriteData(uint8_t *tData, uint8_t numberOfBytesToWrite) {
 
     while (true) {
         if (TWIHS2_IsBusy()) {
+            LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus is busy";
             continue;
         } else {
             if (PCA9685_TWIHS_Write(slaveAddressWrite, tData, numberOfBytesToWrite)) {
                 return true;
             } else {
-                LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus is busy";
+                LOG_INFO << "PCA9685 (" << i2cAddress << ") was not able to perform any transaction: I2C bus NAK";
                 return false;
             }
         }
