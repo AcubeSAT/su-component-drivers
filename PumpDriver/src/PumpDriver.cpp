c1a284e014e78ba6b0c8b8fb4e350566af5b1272
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2024-02-22 22:16:11 +0200

    created pump driver interface

---
+++ PumpDriver/src/PumpDriver.cpp
@@ -0,0 +1,19 @@
+#include "PumpDriver.hpp"
+
+PumpDriver::PumpDriver() = default;
+
+void PumpDriver::init() {
+
+}
+
+void PumpDriver::open() {
+
+}
+
+void PumpDriver::close() {
+
+}
+
+void PumpDriver::setPumpSpeed(uint8_t speed) {
+
+}
