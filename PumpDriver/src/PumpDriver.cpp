aed2db8c304d37414a3b173d3218e704ac3372bd
Giorgos Gkogkou <ggkogkou@ece.auth.gr>
2024-03-04 20:14:48 +0200

    minor change

--- PumpDriver/src/PumpDriver.cpp
+++ PumpDriver/src/PumpDriver.cpp
@@ -3,7 +3,14 @@
 PumpDriver::PumpDriver() = default;
 
 void PumpDriver::init() {
+    pwmGenerator.reset();
 
+    pwmGenerator.setPWMChannel(pwmChannelsConfig[0].pwmChannel, pwmChannelsConfig[0].delayPercent);
+    pwmGenerator.setPWMChannel(pwmChannelsConfig[1].pwmChannel, pwmChannelsConfig[1].delayPercent);
+    pwmGenerator.setPWMChannel(pwmChannelsConfig[2].pwmChannel, pwmChannelsConfig[2].delayPercent);
+    pwmGenerator.setPWMChannel(pwmChannelsConfig[3].pwmChannel, pwmChannelsConfig[3].delayPercent);
+
+    pwmGenerator.exitSleepMode();
 }
 
 void PumpDriver::open() {
