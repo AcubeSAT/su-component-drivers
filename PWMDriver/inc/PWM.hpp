875e745e930167f3270fd6587b93ffabf19566f4
Epatsatzaki <evapatsatzakis@gmail.com>
2024-03-10 19:49:30 +0200

    PWM Driver implementation in su-component-drivers

---
+++ PWMDriver/inc/PWM.hpp
@@ -0,0 +1,59 @@
+//
+// Created by evangelia on 3/10/24.
+//
+
+#include "peripheral/pwm/plib_pwm0.h"
+
+#ifndef SU_COMPONENT_DRIVERS_PWM_HPP
+#define SU_COMPONENT_DRIVERS_PWM_HPP
+
+#endif //SU_COMPONENT_DRIVERS_PWM_HPP
+
+class PWM {
+
+public:
+
+    /*This function initializes given instance of PWM peripheral with the values configured in MCC GUI.
+     * This initializes all the selected channels of given peripheral in MCC GUI.
+     * This function must be called before any other PWM function is called.
+     * This function should only be called once during system initialization.*/
+    static void initializeHeater() {
+        PWM0_Initialize();
+    }
+
+    //This function disables the 2nd channel of the PWM0
+    static void startHeater() {
+        PWM0_ChannelsStart(PWM_CHANNEL_2_MASK);
+    }
+
+    //This function enables the 2nd channel of the PWM0
+    static void stopHeater() {
+        PWM0_ChannelsStop(PWM_CHANNEL_2_MASK);
+    }
+
+    //sets the Duty of PWM
+    void setDutyPercentage(float dutyValuePercentage) {
+        PWM0_ChannelDutySet(PWM_CHANNEL_2, convertDutyCycleToMilliseconds(dutyValuePercentage));
+    }
+
+    /* dutyValue() takes the duty cycle percentage of the waveform as an argument
+     * and returns the on-time of the waveform in milliseconds.
+     */
+    float convertDutyCycleToMilliseconds(float dutyCyclePercentage) {
+        return period * (dutyCyclePercentage / 100.0f);
+    }
+
+
+private:
+    const float period = PWM0_ChannelPeriodGet(PWM_CHANNEL_2);   // equals to the period of the waveform in milliseconds
+
+    /* max electrical input . At the time being, I am not sure if this is needed.
+     * If it is, we should probably initialize the maxPower to the power supply voltage
+     * that we are going to use for the Heater
+     */
+    const float maxPower;  // max electrical input
+
+};
+
+
+#endif //OBC_SOFTWARE_HEATER_H
