b643b99aa6e3d26bd6b2c68eb065fa2d5434e763
Epatsatzaki <evapatsatzakis@gmail.com>
2024-08-24 10:56:30 +0300

    replaced the AFECx macros into their initial form

--- Thermistor/inc/Thermistor.hpp
+++ Thermistor/inc/Thermistor.hpp
@@ -4,28 +4,8 @@
 #include <cmath>
 #include "peripheral/afec/plib_afec_common.h"
 #include "Peripheral_Definitions.hpp"
-
-#if THERMISTOR_PORT == 0
-
 #include "peripheral/afec/plib_afec0.h"
-
-#define AFEC_Initialize AFEC0_Initialize
-#define AFEC_ChannelsEnable AFEC0_ChannelsEnable
-#define AFEC_CallbackRegister AFEC0_CallbackRegister
-#define AFEC_ConversionStart AFEC0_ConversionStart
-#define AFEC_ChannelResultIsReady AFEC0_ChannelResultIsReady
-#define AFEC_ChannelResultGet AFEC0_ChannelResultGet
-
-#elif THERMISTOR_PORT == 1
-
-#include "peripheral/afec/plib_afec0.h"
-#define AFEC_Initialize AFEC1_Initialize
-#define AFEC_ChannelsEnable AFEC1_ChannelsEnable
-#define AFEC_CallbackRegister AFEC1_CallbackRegister
-#define AFEC_ConversionStart AFEC1_ConversionStart
-#define AFEC_ChannelResultIsReady AFEC1_ChannelResultIsReady
-#define AFEC_ChannelResultGet AFEC1_ChannelResultGet
-
+#include "peripheral/afec/plib_afec1.h"
 
 /**
  * Thermistor NRBE10524450B1F driver
@@ -40,6 +20,7 @@ public:
     /**
      * Constructor for the Thermistor class.
      * @param R1, R2, R3: The resistances of the circuit in kilo ohms.
+     * @param AdcChannelNumber Number of the AFEC channel that is being used.
      * @param AdcChannelMask Mask of the AFEC channel that is being used.
      * @note Harmony only lets us use AFEC_CH0_MASK and AFEC_CH1_MASK
      *
@@ -47,14 +28,15 @@ public:
      *
      * @warning if we want to use any of the  channels we need to first enable them from Harmony Configuration
      */
-    Thermistor(float R1, float R2, float R3, AFEC_CHANNEL_MASK AdcChannelMask)
+    Thermistor(float R1, float R2, float R3, AFEC_CHANNEL_NUM AdcChannelNumber, AFEC_CHANNEL_MASK AdcChannelMask)
             : R1(R1),
               R2(R2),
               R3(R3),
-              AdcChannelMask(AdcChannelMask) {}
+              AdcChannelNumber(AdcChannelNumber), AdcChannelMask(AdcChannelMask) {}
 
     /**
      * Constructor for the Thermistor class that takes a default resistor value.
+     * @param AdcChannelNumber Number of the AFEC channel that is being used.
      * @param AdcChannelMask Mask of the AFEC channel that is being used.
      * @note Harmony only lets us use AFEC_CH0_MASK and AFEC_CH1_MASK
      *
@@ -62,7 +44,8 @@ public:
      *
      * @warning if we want to use any of the  channels we need to first enable them from Harmony Configuration
      */
-    Thermistor( AFEC_CHANNEL_MASK AdcChannelMask) :  AdcChannelMask(AdcChannelMask) {}
+    Thermistor(AFEC_CHANNEL_NUM AdcChannelNumber, AFEC_CHANNEL_MASK AdcChannelMask) : AdcChannelNumber(
+            AdcChannelNumber), AdcChannelMask(AdcChannelMask) {}
 
     /**
      *	@return The temperature in Celsius.
@@ -104,6 +87,11 @@ private:
      * Mask of the AFEC peripheral channel being used.
      */
     const AFEC_CHANNEL_MASK AdcChannelMask;
+
+    /**
+     * Number of the AFEC peripheral channel being used.
+     */
+    const AFEC_CHANNEL_NUM AdcChannelNumber;
 
     /**
      * Variable in which the Analog to Digital (ADC) conversion result is stored.
