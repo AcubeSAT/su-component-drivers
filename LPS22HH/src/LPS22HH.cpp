0310a1b6b621bac563832a764d60776c53ad916b
ggkogkou <ggkogkou@ece.auth.gr>
2025-01-07 19:11:05 +0000

    Change LPS22HH protocol to I2C

--- LPS22HH/src/LPS22HH.cpp
+++ LPS22HH/src/LPS22HH.cpp
@@ -1,68 +1,65 @@
 #include "LPS22HH.hpp"
 
-uint8_t LPS22HH::readFromRegister(RegisterAddress registerAddress) const {
-    uint8_t rxData;
+template<typename F, typename... Arguments>
+bool LPS22HH::executeI2CTransaction(F i2cFunction, Arguments... arguments) {
+    if (i2cFunction(I2CAddress, arguments...)) {
+        waitForI2CBuffer();
+        checkForNACK();
+        return true;
+    }
 
-    PIO_PinWrite(ssn, false);
-
-    uint8_t txData = registerAddress | SPI_READ_COMMAND;
-    LPS22HH_SPI_Write(&txData, 1);
-    waitForTransfer();
-
-    LPS22HH_SPI_Read(&rxData, 1);
-    waitForTransfer();
-
-    PIO_PinWrite(ssn, true);
-
-    return rxData;
+    LOG_INFO << "Pressure sensor with address " << static_cast<uint8_t>(I2CAddress) << ": I2C bus is busy";
+    return false;
 }
 
-void LPS22HH::writeToRegister(RegisterAddress registerAddress, uint8_t txData) const {
-    PIO_PinWrite(ssn, false);
+void LPS22HH::checkForNACK() {
+    auto error = LPS22HH_TWIHS_ErrorGet();
+    if (error == TWIHS_ERROR_NACK) {
+        LOG_ERROR << "Pressure sensor with address " << static_cast<uint8_t>(I2CAddress) <<
+                " , is disconnected, suspending task";
+        vTaskSuspend(nullptr);
+    }
+}
 
+void LPS22HH::writeToRegister(RegisterAddress registerAddress, uint8_t txData) {
     uint8_t sendData[2] = {registerAddress, txData};
-    LPS22HH_SPI_Write(sendData, 2);
-    waitForTransfer();
-
-    PIO_PinWrite(ssn, true);
+    executeI2CTransaction(LPS22HH_TWIHS_Write, sendData, sizeof(sendData));
 }
 
-uint8_t LPS22HH::getStatus() {
-    return readFromRegister(STATUS);
+uint8_t LPS22HH::readFromRegister(RegisterAddress registerAddress) {
+    uint8_t registerData = 0;
+    uint8_t registerAddressArray[1] = {registerAddress};
+    executeI2CTransaction(LPS22HH_TWIHS_WriteRead, registerAddressArray, NumberOfBytesInCommand,
+                          &registerData, sizeof(registerData));
+    return registerData;
 }
 
 float LPS22HH::readPressure() {
     triggerOneShotMode();
 
-    int32_t pressureData;
+    const uint8_t pressureOutH = readFromRegister(PRESSURE_OUT_H);
+    const uint8_t pressureOutL = readFromRegister(PRESSURE_OUT_L);
+    const uint8_t pressureOutXL = readFromRegister(PRESSURE_OUT_XL);
 
-    uint8_t pressureOutH = readFromRegister(PRESSURE_OUT_H);
-    uint8_t pressureOutL = readFromRegister(PRESSURE_OUT_L);
-    uint8_t pressureOutXL = readFromRegister(PRESSURE_OUT_XL);
+    const int8_t sign = (pressureOutH & 0b1000'0000) ? 0b1111'1111 : 0;
 
-    int8_t sign = (pressureOutH & 0b10000000) ? 0b11111111 : 0;
+    const int32_t pressureData = (sign << 24) |
+                           static_cast<int32_t>(pressureOutH << 16) |
+                           static_cast<int32_t>(pressureOutL << 8) |
+                           static_cast<int32_t>(pressureOutXL);
 
-    pressureData = (sign << 24) |
-                   static_cast<int32_t> (pressureOutH << 16) |
-                   static_cast<int32_t> (pressureOutL << 8) |
-                   static_cast<int32_t> (pressureOutXL);
-
-    pressureValue = static_cast<float>(pressureData) / PressureSensitivity;
-
-    return pressureValue;
+    return static_cast<float>(pressureData) / PressureSensitivity;
 }
 
 float LPS22HH::readTemperature() {
     triggerOneShotMode();
 
-    uint8_t temperatureOutH = readFromRegister(TEMP_OUT_H);
-    uint8_t temperatureOutL = readFromRegister(TEMP_OUT_L);
+    const uint8_t temperatureOutH = readFromRegister(TEMP_OUT_H);
+    const uint8_t temperatureOutL = readFromRegister(TEMP_OUT_L);
 
     auto signedValue = static_cast<int16_t>(((static_cast<uint16_t>(temperatureOutH) << 8) & 0xFF00) | temperatureOutL);
 
-    temperatureValue = static_cast<float>(signedValue) / TemperatureSensitivity;
-
-    return temperatureValue;
+    return static_cast<float>(signedValue) / TemperatureSensitivity;
 }
 
 void LPS22HH::setODRBits(OutputDataRate rate) {
@@ -89,30 +86,9 @@ void LPS22HH::setFIFOMode(FIFOModes mode) {
 
 void LPS22HH::triggerOneShotMode() {
     /* todo: read CTRL_REG2 first and then write, to preserve any previous config done */
-    uint8_t txData = 0x1; ///> ONE_SHOT bit of CTRL_REG2 register is bit 0
-    writeToRegister(CTRL_REG2, txData);
+    uint8_t oneShotBit = 0x01;
+    writeToRegister(CTRL_REG2, oneShotBit);
 
     vTaskDelay(pdMS_TO_TICKS(500));
-
 }
 
-void LPS22HH::performAreYouAliveCheck() {
-    if (readFromRegister(RegisterAddress::WHO_AM_I) != whoAmIRegisterDefaultValue) {
-        LOG_ERROR << "Pressure Sensor is disconnected, shutting down task";
-        vTaskSuspend(nullptr);
-    }
-    else {
-        LOG_DEBUG << "Pressure Sensor is alive";
-    }
-}
-
-void LPS22HH::waitForTransfer() const {
-    auto start = xTaskGetTickCount();
-    while (LPS22HH_SPI_IsBusy()) {
-        if (xTaskGetTickCount() - start > TimeoutTicks) {
-            LOG_ERROR << "Pressure sensor communication has timed out";
-            break;
-        }
-    }
-
-}
