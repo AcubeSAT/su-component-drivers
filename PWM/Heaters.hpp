da9cdb8bc88318e31d3d48a7b0e609b13c1bcc6e
Georgios Prappas <prappasg@gmail.com>
2025-08-10 14:39:33 +0300

    Update PWM/Heater driver

--- PWM/Heaters.hpp
+++ PWM/Heaters.hpp
@@ -9,11 +9,19 @@ inline constexpr size_t NumberOfHeaters = 4;
 template<PWM_PeripheralID PeripheralID>
 class Heater {
 public:
-    explicit Heater(PWM_PeripheralChannel channel) : pwm(channel) {
-        pwm.setFrequency(PWM_Frequency);
+    /**
+     *
+     * @param channel the PWM channel used by the Heater
+     * @param frequency the PWM frequency in Hz
+     * @param averagePower percentage of duty cycle from 0 to 100
+     */
+    explicit Heater(PWM_PeripheralChannel channel, float frequency,float averagePower) : pwm(channel) {
+        pwm.setFrequency(frequency);
+        setAveragePower(averagePower);
+        close();
+
     }
 
-    static constexpr uint32_t PWM_Frequency = 800;
 
     void open() {
         pwm.start();
@@ -23,7 +31,7 @@ public:
         pwm.stop();
     }
 
-    void setAveragePower(uint32_t averagePower) {
+    void setAveragePower(float averagePower) {
         pwm.setDutyCycle(averagePower);
     }
 
@@ -34,10 +42,13 @@ private:
     PWM<PeripheralID> pwm;
 };
 
+constexpr float DEFAULT_HEATER_FREQUENCY=1500.0f;
+constexpr float DEFAULT_HEATER_DUTY_CYCLE=28.0f;
+
 static_assert(Heaters_PWM_Config.size() == NumberOfHeaters, "PWM channel allocations for heaters are too many");
-inline Heater<Heaters_PWM_Config[0].first> heater1 {Heaters_PWM_Config[0].second};
-inline Heater<Heaters_PWM_Config[1].first> heater2 {Heaters_PWM_Config[1].second};
-inline Heater<Heaters_PWM_Config[2].first> heater3 {Heaters_PWM_Config[2].second};
-inline Heater<Heaters_PWM_Config[3].first> heater4 {Heaters_PWM_Config[3].second};
+inline Heater<Heaters_PWM_Config[0].first> heater1 {Heaters_PWM_Config[0].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
+inline Heater<Heaters_PWM_Config[1].first> heater2 {Heaters_PWM_Config[1].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
+inline Heater<Heaters_PWM_Config[2].first> heater3 {Heaters_PWM_Config[2].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
+inline Heater<Heaters_PWM_Config[3].first> heater4 {Heaters_PWM_Config[3].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
 
 } // namespace PayloadDrivers
