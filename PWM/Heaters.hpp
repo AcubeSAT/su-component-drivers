09ff8beb09864202bee7cf5b56e0813f7e38bab9
Georgios Prappas <prappasg@gmail.com>
2025-08-10 15:31:59 +0300

    Fix style

--- PWM/Heaters.hpp
+++ PWM/Heaters.hpp
@@ -15,7 +15,7 @@ public:
      * @param frequency the PWM frequency in Hz
      * @param averagePower percentage of duty cycle from 0 to 100
      */
-    explicit Heater(PWM_PeripheralChannel channel, float frequency,float averagePower) : pwm(channel) {
+    explicit Heater(PWM_PeripheralChannel channel, float frequency, float averagePower) : pwm(channel) {
         pwm.setFrequency(frequency);
         setAveragePower(averagePower);
         close();
@@ -42,13 +42,13 @@ private:
     PWM<PeripheralID> pwm;
 };
 
-constexpr float DEFAULT_HEATER_FREQUENCY=1500.0f;
-constexpr float DEFAULT_HEATER_DUTY_CYCLE=28.0f;
+constexpr float DefaultHeaterFrequency = 1500.0f;
+constexpr float DefaultHeaterDutyCycle = 28.0f;
 
 static_assert(Heaters_PWM_Config.size() == NumberOfHeaters, "PWM channel allocations for heaters are too many");
-inline Heater<Heaters_PWM_Config[0].first> heater1 {Heaters_PWM_Config[0].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
-inline Heater<Heaters_PWM_Config[1].first> heater2 {Heaters_PWM_Config[1].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
-inline Heater<Heaters_PWM_Config[2].first> heater3 {Heaters_PWM_Config[2].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
-inline Heater<Heaters_PWM_Config[3].first> heater4 {Heaters_PWM_Config[3].second,DEFAULT_HEATER_FREQUENCY,DEFAULT_HEATER_DUTY_CYCLE};
+inline Heater<Heaters_PWM_Config[0].first> heater1 {Heaters_PWM_Config[0].second, DefaultHeaterFrequency,  DefaultHeaterDutyCycle};
+inline Heater<Heaters_PWM_Config[1].first> heater2 {Heaters_PWM_Config[1].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
+inline Heater<Heaters_PWM_Config[2].first> heater3 {Heaters_PWM_Config[2].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
+inline Heater<Heaters_PWM_Config[3].first> heater4 {Heaters_PWM_Config[3].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
 
 } // namespace PayloadDrivers
