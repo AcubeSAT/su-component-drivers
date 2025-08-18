aa7af6c507f74e7621c70d572280cb210120e965
Georgios Prappas <prappasg@gmail.com>
2025-08-18 16:10:17 +0300

    Fix Heater duty cycle

--- PWM/Heaters.hpp
+++ PWM/Heaters.hpp
@@ -10,15 +10,15 @@ template<PWM_PeripheralID PeripheralID>
 class Heater {
 public:
     /**
-     *
+     *@brief Initializes the Heater and underlying PWM object, making it closed by default and setting the appropriate PWM frequency and duty cycle
      * @param channel the PWM channel used by the Heater
      * @param frequency the PWM frequency in Hz
      * @param dutyCycle percentage of duty cycle from 0 to 100
      */
     explicit Heater(PWM_PeripheralChannel channel, float frequency, float dutyCycle) : pwm(channel) {
+        close();
         pwm.setFrequency(frequency);
         setDutyCycle(dutyCycle);
-        close();
 
     }
 
@@ -49,15 +49,13 @@ private:
      * Templated PWM instance for the given peripheral.
      */
     PWM<PeripheralID> pwm;
+
 };
 
-constexpr float DefaultHeaterFrequency = 1500.0f;
-constexpr float DefaultHeaterDutyCycle = 28.0f;
-
 static_assert(Heaters_PWM_Config.size() == NumberOfHeaters, "PWM channel allocations for heaters are too many");
-inline Heater<Heaters_PWM_Config[0].first> heater1 {Heaters_PWM_Config[0].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
-inline Heater<Heaters_PWM_Config[1].first> heater2 {Heaters_PWM_Config[1].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
-inline Heater<Heaters_PWM_Config[2].first> heater3 {Heaters_PWM_Config[2].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
-inline Heater<Heaters_PWM_Config[3].first> heater4 {Heaters_PWM_Config[3].second, DefaultHeaterFrequency, DefaultHeaterDutyCycle};
+inline etl::optional<Heater<Heaters_PWM_Config[0].first>> heater1;
+inline etl::optional<Heater<Heaters_PWM_Config[1].first>> heater2;
+inline etl::optional<Heater<Heaters_PWM_Config[2].first>> heater3;
+inline etl::optional<Heater<Heaters_PWM_Config[3].first>> heater4;
 
 } // namespace PayloadDrivers
