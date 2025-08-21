07a0315783ca4b88830243aea214f1b96be423be
Georgios Prappas <prappasg@gmail.com>
2025-08-21 18:48:53 +0300

    Fix Style

--- FGDOS/FGDOS.hpp
+++ FGDOS/FGDOS.hpp
@@ -35,67 +35,63 @@
  * @class FGDOS
  * @brief Interface for communicating with the Floating Gate Dosimeter (FGDOS) radiation sensors.
  */
-class FGDOS
-{
-    constexpr static uint8_t writeMask=0b0100'0000;
-    constexpr static uint8_t readMask=0b1000'0000;
-    //private struct that prepares config data to be written to registers
+class FGDOS {
+    constexpr static uint8_t writeMask = 0b0100'0000;
+    constexpr static uint8_t readMask = 0b1000'0000;
     /**
      * @struct ConfigData
      * Used to stage the configuration data to be written with SPI
      */
-    struct ConfigData
-    {
-        etl::array<uint8_t, 7> data{
-            writeMask|0x9//write base address
-           ,0b0001'0110//0x9:default target value for default clock and window and high sensitivity, tdiv=1: 90kHz -> 22 as specified by manufacturer
+    struct ConfigData {
+        etl::array<uint8_t, 7> data {
+            writeMask | 0x9//write base address
+           ,0b0001'0110 //0x9:default target value for default clock and window and high sensitivity, tdiv=1: 90kHz -> 22 as specified by manufacturer
            ,0b0000'1100 //0xA:default threshold value for -||- : 50kHZ -> 12 -||-
-           ,0b1100'0000//0xB:experimental configuration
-           ,0b0111'1001//0xC:high sensitivity
-           ,0 //0xD:start with charging disabled, charge voltage is 000->14.5V
-           ,0b0000'0100//0xE dont measure during spi: true, interrupt:0, engate:0
+           ,0b1100'0000 //0xB:experimental configuration
+           ,0b0111'1001 //0xC:high sensitivity
+           ,0           //0xD:start with charging disabled, charge voltage is 000->14.5V
+           ,0b0000'0100 //0xE dont measure during spi: true, interrupt:0, engate:0
        };
 
         /**
          * Used to enable recharge through a direct write to the appropriate register
          * @return the configuration byte that has the recharge enable bit set to true and the rest of the bits unaltered
          */
-        [[nodiscard]] uint8_t getRechargeEnableByte() const{
-            return data[5]|0b0100'0000;
+        [[nodiscard]] uint8_t getRechargeEnableByte() const {
+            return data[5] | 0b0100'0000;
         }
-        //returns byte that must be written to 0xA to change threshold to required value without changing anything else
         /**
          * Used to edit threshold frequency through a direct write to the appropriate register
          * @param threshold5Bits 5bit value that corresponds to the frequency , acquired through frequencyTo5Bit()
          * @return The appropriate configuration byte, with the threshold frequency altered to match the specified value and the rest of the bits unaltered
          */
-        [[nodiscard]] uint8_t getThresholdByte(const uint8_t threshold5Bits) const{
-            return (data[2]&0b1110'0000)|threshold5Bits;
+        [[nodiscard]] uint8_t getThresholdByte(const uint8_t threshold5Bits) const {
+            return (data[2] & 0b1110'0000) | threshold5Bits;
         }
         /**
             * Sets the Target frequency
             * @param freq5bits 5 bit value that corresponds to the frequency, acquired through frequencyTo5Bit()
         */
-        inline void setTargetFrequency(const uint8_t freq5bits){
-            configASSERT(freq5bits<=0b11111);
-            data[1]=(data[1]&0b1110'0000)|freq5bits;
+        inline void setTargetFrequency(const uint8_t freq5bits) {
+            configASSERT(freq5bits <= 0b11111);
+            data[1] = (data[1] & 0b1110'0000) | freq5bits;
         }
 
         /**
          * Sets the Threshold frequency
          * @param freq5bits 5 bit value that corresponds to the frequency, acquired through frequencyTo5Bit()
          */
-        inline void setThresholdFrequency(const uint8_t freq5bits){
-            configASSERT(freq5bits<=0b1111);
-            data[2]=(data[2]&0b1110'0000)|freq5bits;
+        inline void setThresholdFrequency(const uint8_t freq5bits) {
+            configASSERT(freq5bits <= 0b1111);
+            data[2] = (data[2] & 0b1110'0000) | freq5bits;
         }
 
         /**
          * Sets the sensitivity to high or low. The user must set target and threshold frequencies separately
          * @param high true if sensitivity is high, false if low
          */
-        inline void setSensitivity(const bool high){
-            data[4]=(data[4]&0b1111'1000)|(high?0b001:0b100);
+        inline void setSensitivity(const bool high) {
+            data[4] = (data[4] & 0b1111'1000) | (high ? 0b001 : 0b100);
 
         }
 
@@ -104,9 +100,9 @@ class FGDOS
          *Sets the recharge voltage
          * @param voltage3bits A 3 bit value which is translated to a value between 14.5 and 18 Volts
          */
-        inline void setVoltage(const uint8_t voltage3bits){
-            configASSERT(voltage3bits<=0b111);
-            data[5]=(data[5]&0b1111'1000)|(voltage3bits);
+        inline void setVoltage(const uint8_t voltage3bits) {
+            configASSERT(voltage3bits <= 0b111);
+            data[5] = (data[5] & 0b1111'1000) | (voltage3bits);
 
         }
         /**
@@ -115,7 +111,7 @@ class FGDOS
          */
         inline void setTDiv(bool tDiv)
         {
-            data[3]=(data[3]&0b1111'1110)|tDiv;
+            data[3] = (data[3] & 0b1111'1110) | tDiv;
         }
     };
 
@@ -139,7 +135,7 @@ public:
      *
      *@param temperatureOffset value provided by calibration to correctly calculate the temperature
      */
-    FGDOS(uint32_t clockFrequency,PIO_PIN chipSelectPin, bool highSensitivity,uint8_t chargeVoltage,int8_t temperatureOffset);
+    FGDOS(uint32_t clockFrequency, PIO_PIN chipSelectPin, bool highSensitivity, uint8_t chargeVoltage, int8_t temperatureOffset);
 
     /**
      * @brief Retrieves the Chip ID from the FGDOS device.
@@ -160,10 +156,9 @@ public:
      * @return The signed temperature value in degrees Celsius
      */
     //TODO: calibration
-    [[nodiscard]] int getTemperature()
-    {
+    [[nodiscard]] int getTemperature() {
         calculateDose();
-        return temperature+temperatureOffset;
+        return temperature + temperatureOffset;
 
     }
 
@@ -188,12 +183,12 @@ private:
     /**
      * Constant temperature offset, provided by calibration
      */
-    int8_t temperatureOffset=0;
+    int8_t temperatureOffset = 0;
 
     /**
      * Timestamp of last read
      */
-    uint32_t lastReadTick =0;
+    uint32_t lastReadTick = 0;
 
     /**
      * cached temperature value
@@ -207,75 +202,75 @@ private:
     /**
      * cached Reference Frequency value
      */
-    uint32_t refFrequency=0;
+    uint32_t refFrequency = 0;
     /**
      * cached Sensor Frequency value
      */
-    uint32_t sensorFrequency=0;
+    uint32_t sensorFrequency = 0;
 
     /**
      * Sensor Frequency from the previous read, used for dose increase calculations
      */
-    uint32_t sensorPrevFrequency=0;
+    uint32_t sensorPrevFrequency = 0;
     /**
         * Sensor Reference from the previous read, used for approximation if recharges occurred
      */
-    uint32_t refPrevFrequency=0;
+    uint32_t refPrevFrequency = 0;
 
     /**
      * Flag corresponding to the device's sensor overflow flag
      */
-    bool sensorOverflown=false;
+    bool sensorOverflown = false;
 
     /**
      * Flag corresponding to the device's reference overflow flag
      */
-    bool referenceOverflown=false;
+    bool referenceOverflown = false;
 
     /**
      * Flag corresponding to the device's recharge in progress flag
      */
-    bool recharging=false;
+    bool recharging = false;
 
     /**
      * Accumulated dose of all previous reads except the last one
      */
-    float previousDose=0.0f;
+    float previousDose = 0.0f;
     /**
      * Dose increase, calculated fron the last read
      */
-    float doseIncrease=0.0f;
+    float doseIncrease = 0.0f;
 
     /**
      * Hardware clock frequency
      */
-    uint32_t configClockFrequency=0;
+    uint32_t configClockFrequency = 0;
 
     /**
      * Window amount value. Currently only the default value is supported.
      */
-    uint32_t windowAmount=32768;
+    uint32_t windowAmount = 32768;
 
 
-    constexpr static uint32_t thresholdFrequencyDefaultHigh=50'000;
+    constexpr static uint32_t thresholdFrequencyDefaultHigh = 50'000;
 
-    constexpr static uint32_t targetFrequencyDefaultHigh=90'000;
+    constexpr static uint32_t targetFrequencyDefaultHigh = 90'000;
 
-    constexpr static uint32_t thresholdFrequencyDefaultLow=140'000;
+    constexpr static uint32_t thresholdFrequencyDefaultLow = 140'000;
 
-    constexpr static uint32_t targetFrequencyDefaultLow=180'000;
+    constexpr static uint32_t targetFrequencyDefaultLow = 180'000;
 
     //50k for high sensitivity, 140k for low. Target frequency is 40k above threshold
-    uint32_t thresholdFrequency=0;
-    uint32_t targetFrequency=0;
+    uint32_t thresholdFrequency = 0;
+    uint32_t targetFrequency = 0;
 
     //10k for low and 70k for high
 
-    float configSensitivity=0.0f;
-    constexpr static float sensitivityDefaultLow=10'000.0f;
-    constexpr static float sensitivityDefaultHigh=70'000.0f;
+    float configSensitivity = 0.0f;
+    constexpr static float sensitivityDefaultLow = 10'000.0f;
+    constexpr static float sensitivityDefaultHigh = 70'000.0f;
     //placeholder value for linear coefficient that related reference frequency to temperature
-    constexpr static float configCoefTempToRef=0.0f;
+    constexpr static float configCoefTempToRef = 0.0f;
 
 
 
@@ -283,7 +278,7 @@ private:
      * Whether target and threshold values have smaller ranges (*1024 instead of *8912).
      * Currently force to true for high sensitivity for accuracy, and forced to false for low sensitivity to prevent overflow
      */
-    bool tDiv=false;
+    bool tDiv = false;
 
     /**
      * SPI Write Wrapper
@@ -291,11 +286,10 @@ private:
      * @param size size of data to be written
      * @return true if successful
      */
-    [[nodiscard]] inline bool write(uint8_t* data,size_t size) const
-    {
+    [[nodiscard]] inline bool write(uint8_t* data,size_t size) const {
         PIO_PinWrite(ChipSelectPin, false);
 
-        const bool success=FGDOS_Write(data,size);
+        const bool success=FGDOS_Write(data, size);
         PIO_PinWrite(ChipSelectPin, true);
         return success;
 
@@ -309,13 +303,12 @@ private:
      * @param readSize size of data to be read
      * @return true if successful
      */
-    [[nodiscard]] inline bool writeRead(uint8_t* writeData,size_t writeSize,uint8_t* readData,size_t readSize)const
-    {
+    [[nodiscard]] inline bool writeRead(uint8_t* writeData,size_t writeSize,uint8_t* readData,size_t readSize) const {
 
 
         PIO_PinWrite(ChipSelectPin, false);
 
-        const bool success=FGDOS_WriteRead(writeData,writeSize,readData,readSize);
+        const bool success=FGDOS_WriteRead(writeData, writeSize, readData, readSize);
         PIO_PinWrite(ChipSelectPin, true);
         return success;
 
@@ -325,17 +318,18 @@ private:
      * Check if enough time is elapsed to read again.
      * @return false if not enough time is elapsed to try reading
      */
-    [[nodiscard]] bool enoughTimeElapsed() const
-    {
+    [[nodiscard]] bool enoughTimeElapsed() const {
         float elapsed = getSecondsPassed();
-        return  (getSecondsPassed()> static_cast<float>(windowAmount)/static_cast<float>(configClockFrequency));
+        return  (getSecondsPassed() > static_cast<float>(windowAmount) / static_cast<float>(configClockFrequency));
 
     }
     /**
      * Helper function to check time elapsed since last read
      * @return the seconds passed from last read
      */
-    [[nodiscard]] inline float getSecondsPassed() const{return (static_cast<float>(xTaskGetTickCount()-lastReadTick))/configTICK_RATE_HZ;}
+    [[nodiscard]] inline float getSecondsPassed() const {
+        return (static_cast<float>(xTaskGetTickCount() - lastReadTick)) / configTICK_RATE_HZ;
+    }
 
 
     /**
@@ -350,8 +344,7 @@ private:
     /**
      * Helper Function that updates @ref lastReadTick
      */
-    inline void updateLastReadTick()
-    {
+    inline void updateLastReadTick() {
         lastReadTick=xTaskGetTickCount();
 
     };
@@ -380,7 +373,7 @@ private:
      * @param highSensitivity true for high sensitivity mode, false for low
      * @param forceRecharge if true, an initial recharge will occur. Recommended for first use after installation
      */
-    void initConfiguration(uint8_t chargeVoltage,bool highSensitivity, bool forceRecharge);
+    void initConfiguration(uint8_t chargeVoltage, bool highSensitivity, bool forceRecharge);
 
     /**
      * Converts frequency to 5bit value to be used in target and threshold registers
@@ -390,13 +383,13 @@ private:
     [[nodiscard]] uint8_t frequencyTo5Bit(const uint32_t frequency) const{
         //convert to 5 bit values: new=old*window/(tDiv?(clock_freq*8192):clock_freq*1024)
         //use rounding for better accuracy
-        const auto numerator=((frequency*windowAmount));
-        const auto denominator=(configClockFrequency*(tDiv?1024:8192));
-       const auto result=numerator/denominator+(2*(numerator%denominator)>=(denominator));
+        const auto numerator = ((frequency*windowAmount));
+        const auto denominator = (configClockFrequency*(tDiv?1024:8192));
+        const auto result = numerator / denominator + (2 * (numerator % denominator) >= (denominator));
 
     //configASSERT that result is 5 bit value
-        configASSERT(result<=0b0001'1111);
-    return static_cast<uint8_t>(result)&0b0001'1111;
+    configASSERT(result <= 0b0001'1111);
+    return static_cast<uint8_t>(result) & 0b0001'1111;
 
 }
 
@@ -407,7 +400,7 @@ private:
 
     /**
      *Only for testing
-     *Prints all values from data and config registers, does not update cached data
+     *Prints all values from data and coWnfig registers, does not update cached data
      */
     void debugPrintAll() const;
 
