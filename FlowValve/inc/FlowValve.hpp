c68e30085dfb0bf0a8b45a264b5827f89ebb9818
Georgios Prappas <prappasg@gmail.com>
2025-09-27 06:27:34 +0000

    Valve driver additions

--- FlowValve/inc/FlowValve.hpp
+++ FlowValve/inc/FlowValve.hpp
@@ -4,6 +4,8 @@
 #include "peripheral/pio/plib_pio.h"
 #include "FreeRTOS.h"
 #include "Task.hpp"
+#include "semphr.h"
+#include "InternalFlash.hpp"
 
 /**
  * @class FlowValve
@@ -12,40 +14,6 @@
  */
 class FlowValve {
 public:
-    /**
-     * Class constructor.
-     *
-     * @param openPin The GPIO pin number for opening the valve.
-     * @param closePin The GPIO pin number for closing the valve.
-     */
-    explicit FlowValve(PIO_PIN openPin, PIO_PIN closePin) : OpenPin(openPin), ClosePin(closePin) {
-        PIO_PinWrite(OpenPin, false);
-        PIO_PinWrite(ClosePin, false);
-    };
-
-    /**
-     * Function that opens the valve by sending a latching pulse to the open pin.
-     * A delay is used to ensure smooth transition to the next latching state.
-     */
-    void openValve() const;
-
-    /**
-     * Function that closes the valve by sending a latching pulse to the close pin.
-     * A delay is used to ensure smooth transition to the next latching state.
-     */
-    void closeValve() const;
-
-    /**
-     * The GPIO pin number for opening the valve.
-     */
-    const PIO_PIN OpenPin = PIO_PIN_NONE;
-
-    /**
-     * The GPIO pin number for closing the valve.
-     */
-    const PIO_PIN ClosePin = PIO_PIN_NONE;
-
-private:
     /**
      * @enum ValveState
      *
@@ -57,8 +25,130 @@ private:
     };
 
     /**
+     * Class constructor.
+     * @note Constructs and gives the semaphore if not already done before (Singleton Pattern)
+     * @param openPin The GPIO pin number for opening the valve.
+     * @param closePin The GPIO pin number for closing the valve.
+     * @param index Index of this valve, from 0 to 7. Used to save its state
+     */
+    explicit FlowValve(PIO_PIN openPin, PIO_PIN closePin, uint8_t index) : OpenPin(openPin), ClosePin(closePin), valveIndex{index} {
+        PIO_PinWrite(OpenPin, false);
+        PIO_PinWrite(ClosePin, false);
+        if (not flash.has_value()) {
+            flash.emplace();
+        }
+        if (index >= valveStates.size()) {
+            LOG_ERROR << "Invalid valve index";
+            return;
+        }
+        if (not valveSemaphore) {
+            valveSemaphore.emplace(xSemaphoreCreateBinaryStatic(&valveSemaphoreBuffer));
+            xSemaphoreGive(valveSemaphore.value());
+        }
+        auto savedState = getValveStateFromFlash(index);
+        if (!savedState) {
+            valveStates[index] = ValveState::OPEN;
+            closeValve();
+        } else {
+
+            valveStates[index] = savedState.value();
+        }
+
+    };
+
+    /**
+     * Function that opens the valve by sending a latching pulse to the open pin.
+     * A delay is used to ensure smooth transition to the next latching state.
+     * @note An indefinite wait to take the valve semaphore is included for thread safety. It is released only after the operation has finished
+     * @return false if the valve was already open or invalid
+     */
+    [[nodiscard]] bool openValve() const;
+
+    /**
+     * Function that closes the valve by sending a latching pulse to the close pin.
+     * A delay is used to ensure smooth transition to the next latching state.
+     * @note An indefinite wait to take the valve semaphore is included for thread safety. It is released only after the operation has finished
+     * @return false if the valve was already closed or invalid
+     */
+    [[nodiscard]] bool closeValve() const;
+
+    /**
+     * Gets the cached state of the valve
+     * @return nullopt if the valve is invalid, opened or closed otherwise
+     */
+    [[nodiscard]] etl::optional<ValveState> getValveState() const {
+        if (valveIndex >= valveStates.size()) {
+            return etl::nullopt;
+        }
+        return valveStates[valveIndex];
+    }
+
+    /**
+     * The GPIO pin number for opening the valve.
+     */
+    const PIO_PIN OpenPin = PIO_PIN_NONE;
+
+    /**
+     * The GPIO pin number for closing the valve.
+     */
+    const PIO_PIN ClosePin = PIO_PIN_NONE;
+
+
+private:
+    /**
+     * Flash driver instance. Initialized with the Singleton Pattern
+     */
+    inline static etl::optional<FlashDriver> flash{};
+
+    /**
+     * Semaphore used to protect valve operations
+     */
+    inline static etl::optional<SemaphoreHandle_t> valveSemaphore;
+
+    /**
+     * Buffer for valve semaphore
+     */
+    inline static StaticSemaphore_t valveSemaphoreBuffer;
+
+    /**
+     * Index of this valve. Used to save its state
+     */
+    const uint8_t valveIndex;
+
+    /**
+     * Array of all valve states. Populated by reading from internal flash and
+     */
+    inline static etl::array<ValveState, 8> valveStates{ValveState::OPEN, ValveState::OPEN, ValveState::OPEN, ValveState::OPEN, ValveState::OPEN, ValveState::OPEN, ValveState::OPEN, ValveState::OPEN};
+
+    /**
+     * Retreives the valve state from internal flash
+     * @param index valve index from 0 to 7
+     * @return The valve state, either open or closed. In case of read error, nullopt
+     */
+    static etl::optional<ValveState> getValveStateFromFlash(uint8_t index);
+    /**
+     * Stores the valve state to internal flash. In case of invalid index ore error the state is not saved
+     * @param index the valve index from 0 to 7
+     * @param state the state, either open or closed
+     */
+    static void setValveStateToFlash(uint8_t index, ValveState state);
+
+    /**
      * Pulse's time duration for latching operation in milliseconds.
      */
     static constexpr uint16_t LatchPulseDuration = 500;
-
+    /**
+     * Address to store valve state in internal flash
+     */
+    static constexpr uint32_t InternalFlashStorageAddress = 0x5F0000;
+    /**
+     * Arbitrary value to store if a valve is closed
+     * @note this value was chosen to make it difficult for bitflips to change it
+     */
+    static constexpr uint8_t ValveClosedMagicNumber = 0b10101010;
+    /**
+     * Arbitrary value to store if a valve is closed
+     * @note this value was chosen to make it difficult for bitflips to change it
+     */
+    static constexpr uint8_t ValveOpenMagicNumber = 0b11001100;
 };
--- FlowValve/src/FlowValve.cpp
+++ FlowValve/src/FlowValve.cpp
@@ -1,13 +1,80 @@
 #include "FlowValve.hpp"
 
-void FlowValve::openValve() const {
+bool FlowValve::openValve() const {
+    if (valveIndex >= valveStates.size()) {
+        LOG_ERROR << "Valve index invalid!";
+        return false;
+    }
+    xSemaphoreTake(valveSemaphore.value(), portMAX_DELAY);
+    if (valveStates[valveIndex] == ValveState::OPEN) {
+        xSemaphoreGive(valveSemaphore.value());
+        LOG_ERROR << "Valve is already open!";
+        return false;
+    }
     PIO_PinWrite(OpenPin, true);
     vTaskDelay(pdMS_TO_TICKS(LatchPulseDuration));
     PIO_PinWrite(OpenPin, false);
+    setValveStateToFlash(valveIndex, ValveState::OPEN);
+    valveStates[valveIndex] = ValveState::OPEN;
+    xSemaphoreGive(valveSemaphore.value());
+
+    return true;
 }
 
-void FlowValve::closeValve() const {
+bool FlowValve::closeValve() const {
+    if (valveIndex >= valveStates.size()) {
+        LOG_ERROR << "Valve index invalid!";
+        return false;
+    }
+    xSemaphoreTake(valveSemaphore.value(), portMAX_DELAY);
+    if (valveStates[valveIndex] == ValveState::CLOSED) {
+        xSemaphoreGive(valveSemaphore.value());
+        LOG_ERROR << "Valve is already closed!";
+        return false;
+    }
     PIO_PinWrite(ClosePin, true);
     vTaskDelay(pdMS_TO_TICKS(LatchPulseDuration));
     PIO_PinWrite(ClosePin, false);
+    setValveStateToFlash(valveIndex, ValveState::CLOSED);
+    valveStates[valveIndex] = ValveState::CLOSED;
+    xSemaphoreGive(valveSemaphore.value());
+    return true;
+}
+
+etl::optional<FlowValve::ValveState> FlowValve::getValveStateFromFlash(uint8_t index) {
+    if (index >= valveStates.size()) {
+        LOG_ERROR << "Valve index invalid!";
+        return etl::nullopt;
+    }
+    etl::array<uint32_t, 2> savedValveStates{};
+    auto result = flash->readFromMemory(savedValveStates, sizeof(uint32_t) * savedValveStates.size(), InternalFlashStorageAddress);
+    if (result != FlashDriver::EFCError::NONE) {
+        LOG_ERROR << "Flash Read Error:" << static_cast<int>(result);
+        return etl::nullopt;
+    }
+    uint8_t state = reinterpret_cast<uint8_t*>(savedValveStates.data())[index];
+    if (state == ValveClosedMagicNumber) {
+        return ValveState::CLOSED;
+    } else if (state != ValveOpenMagicNumber) {
+        LOG_ERROR << "Read Invalid valve state. Assuming Open!";
+    }
+    return ValveState::OPEN;
+}
+
+void FlowValve::setValveStateToFlash(uint8_t index, ValveState state) {
+    if (index >= valveStates.size()) {
+        LOG_ERROR << "Invalid Valve Index";
+        return;
+    }
+    etl::array<uint32_t, 4> savedValveStates{};
+    auto readResult = flash->readFromMemory(savedValveStates, sizeof(uint32_t) * savedValveStates.size(), InternalFlashStorageAddress);
+    if (readResult != FlashDriver::EFCError::NONE) {
+        LOG_ERROR << "Flash Read Error before Write:" << static_cast<int>(readResult);
+    }
+    auto& savedState = reinterpret_cast<uint8_t*>(savedValveStates.data())[index];
+    savedState = ((state == ValveState::OPEN) ? ValveOpenMagicNumber : ValveClosedMagicNumber);
+    auto writeResult = flash->writeQuadWord(savedValveStates, InternalFlashStorageAddress);
+    if (writeResult != FlashDriver::EFCError::NONE) {
+        LOG_ERROR << "Flash write Error:" << static_cast<int>(writeResult);
+    }
 }
